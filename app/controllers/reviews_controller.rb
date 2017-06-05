0213bbc173547e728cc8e5e392596c2d255c0d39
Inge Wieser <ingewieser@gmail.com>
2017-06-05 14:44:37 -0700

    Add user authorization for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,14 @@
 class ReviewsController < ApplicationController
 
-  before_action :authenticate_user! # nu zouden niet ingelogde users geen reviews mogen creeren of deleten
+  before_action :authenticate_user!, only: [:create, :destroy] # nu zouden niet ingelogde users geen reviews mogen creeren of deleten
 
   def create
     @idea = Idea.find params[:idea_id]
     review_params = params.require(:review).permit(:body)
     @review = Review.new review_params
     @review.idea = @idea
+    @review.user = current_user
+
     if @review.save
       redirect_to idea_path(@idea), notice: "Review created!"
     else
