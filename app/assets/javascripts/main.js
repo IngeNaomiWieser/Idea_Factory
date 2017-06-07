948ccec5489df0e9d84afeaea108cbf32c648a6d
Inge Wieser <ingewieser@gmail.com>
2017-06-06 22:28:53 -0700

    Make clicking on a button show/hide body of title

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,16 @@
-$(document).ready(function() {
+// TO SLIDE WHEN THE TITLE IS CLICKED
+// $(document).ready(function() {
   // $('.idea-container').on('click', function(event) {
   //   event.preventDefault();
   //   $(this).children().eq(1).slideToggle();
   // });
+// });
+
+
+// TO SLIDE WHEN THE SHOW BUTTON IS CLICKED
+$(document).ready(function() {
+  $('.show_button').on('click', function(event) {
+    $(this).parent().siblings().slideToggle();
+    $(this).html( $(this).text() == 'Hide' ? 'Show' : 'Hide' );
+  });
 });
